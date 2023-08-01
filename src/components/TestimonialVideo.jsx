af47f830443151dfd8914687937499bbc5b70244
Gerald <codyhenry527@gmail.com>
2023-08-01 11:28:47 -0400

    add new component for video

---
+++ src/components/TestimonialVideo.jsx
@@ -0,0 +1,14 @@
+export default function TestimonialVideo() {
+  return (
+    <div className="testimonials-container">
+      <video
+        width="640"
+        height="480"
+        controls
+        preload="auto"
+        playsInline
+        src="/Testimonials.mp4"
+      ></video>
+    </div>
+  )
+}
